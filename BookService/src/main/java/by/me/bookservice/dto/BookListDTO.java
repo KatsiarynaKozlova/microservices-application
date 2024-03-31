12e1516d6b6bbe1610f6600e5adab29b6408ff54
Katsiaryna Kozlova <katekozlova1411@gmail.com>
2024-03-31 13:54:27 +0300

    update bookservice

--- BookService/src/main/java/by/me/bookservice/dto/BookListDTO.java
+++ BookService/src/main/java/by/me/bookservice/dto/BookListDTO.java
@@ -1,12 +1,12 @@
 package by.me.bookservice.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import lombok.Data;
 
 import java.util.List;
 
 @AllArgsConstructor
-@NoArgsConstructor
+@Data
 public class BookListDTO {
     private List<BookDTO> bookDTOList;
 }
