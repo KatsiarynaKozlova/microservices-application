a3f781b725477664c2f0297e9a3d2d26e0e4ace0
Katsiaryna Kozlova <katekozlova1411@gmail.com>
2024-04-04 13:31:55 +0300

    update controller

--- BookService/src/main/java/by/me/bookservice/controller/BookController.java
+++ BookService/src/main/java/by/me/bookservice/controller/BookController.java
@@ -27,21 +27,13 @@ public class BookController {
     }
 
     @GetMapping("/isbn/{isbn}")
-    public ResponseEntity<BookDTO> getBookByIsbn(@PathVariable String isbn) {
-        try {
-            return ResponseEntity.ok().body(bookService.getBookByIsbn(isbn));
-        } catch (BookNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
+    public ResponseEntity<BookDTO> getBookByIsbn(@PathVariable String isbn) throws BookNotFoundException {
+        return ResponseEntity.ok().body(bookService.getBookByIsbn(isbn));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<BookDTO> getAllBookByIsbn(@PathVariable Long id) {
-        try {
-            return ResponseEntity.ok().body(bookService.getBookById(id));
-        } catch (BookNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
+    public ResponseEntity<BookDTO> getAllBookByIsbn(@PathVariable Long id) throws BookNotFoundException {
+        return ResponseEntity.ok().body(bookService.getBookById(id));
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
@@ -50,13 +42,8 @@ public class BookController {
         bookService.deleteBookById(id);
     }
 
-
     @PutMapping("/{id}")
-    public ResponseEntity<BookDTO> updateBookById(@PathVariable Long id, @RequestBody BookDTO bookDTO) {
-        try {
-            return ResponseEntity.ok().body(bookService.updateBook(id, bookDTO));
-        } catch (BookNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
+    public ResponseEntity<BookDTO> updateBookById(@PathVariable Long id, @RequestBody BookDTO bookDTO) throws BookNotFoundException {
+        return ResponseEntity.ok().body(bookService.updateBook(id, bookDTO));
     }
 }
